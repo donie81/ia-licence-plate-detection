fb67718def75e594c2714ce8d81e70cc0a6596dc
karan10037 <karan10037@iiitd.ac.in>
2012-12-02 15:03:46 +0000

    open dialog made

--- IAproject/Form1.Designer.cs
+++ IAproject/Form1.Designer.cs
@@ -35,7 +35,14 @@
             this.imageBox5 = new Emgu.CV.UI.ImageBox();
             this.imageBox6 = new Emgu.CV.UI.ImageBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
@@ -46,10 +53,9 @@
             // 
             // imageBox2
             // 
-            this.imageBox2.Location = new System.Drawing.Point(440, 12);
+            this.imageBox2.Location = new System.Drawing.Point(459, 12);
             this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(424, 358);
-            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox2.Size = new System.Drawing.Size(430, 320);
             this.imageBox2.TabIndex = 2;
             this.imageBox2.TabStop = false;
             // 
@@ -57,70 +63,132 @@
             // 
             this.imageBox1.Location = new System.Drawing.Point(12, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(385, 367);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox1.Size = new System.Drawing.Size(430, 320);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
             // imageBox3
             // 
-            this.imageBox3.Location = new System.Drawing.Point(908, 12);
+            this.imageBox3.Location = new System.Drawing.Point(908, 9);
             this.imageBox3.Name = "imageBox3";
-            this.imageBox3.Size = new System.Drawing.Size(87, 49);
-            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox3.Size = new System.Drawing.Size(430, 320);
             this.imageBox3.TabIndex = 2;
             this.imageBox3.TabStop = false;
             // 
             // imageBox4
             // 
-            this.imageBox4.Location = new System.Drawing.Point(12, 398);
+            this.imageBox4.Location = new System.Drawing.Point(12, 353);
             this.imageBox4.Name = "imageBox4";
-            this.imageBox4.Size = new System.Drawing.Size(85, 70);
-            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox4.Size = new System.Drawing.Size(430, 320);
             this.imageBox4.TabIndex = 2;
             this.imageBox4.TabStop = false;
             // 
             // imageBox5
             // 
-            this.imageBox5.Location = new System.Drawing.Point(440, 398);
+            this.imageBox5.Location = new System.Drawing.Point(459, 352);
             this.imageBox5.Name = "imageBox5";
-            this.imageBox5.Size = new System.Drawing.Size(112, 70);
-            this.imageBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox5.Size = new System.Drawing.Size(430, 320);
             this.imageBox5.TabIndex = 2;
             this.imageBox5.TabStop = false;
             // 
             // imageBox6
             // 
-            this.imageBox6.Location = new System.Drawing.Point(908, 398);
+            this.imageBox6.Location = new System.Drawing.Point(908, 353);
             this.imageBox6.Name = "imageBox6";
-            this.imageBox6.Size = new System.Drawing.Size(395, 306);
-            this.imageBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox6.Size = new System.Drawing.Size(430, 320);
             this.imageBox6.TabIndex = 2;
             this.imageBox6.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(935, 356);
+            this.button1.Location = new System.Drawing.Point(1151, 331);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load Image";
             this.button1.UseVisualStyleBackColor = true;
-            //this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // openFileDialog1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1067, 359);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1232, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Detect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(578, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "hist_eq then val_eq \'ed image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1017, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sobeled image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(144, 677);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Binarized and dilated image";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(578, 677);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result after applying canny()";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1017, 677);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Result after applying getregion()";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1362, 742);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.imageBox6);
             this.Controls.Add(this.imageBox5);
@@ -151,7 +219,14 @@
         private Emgu.CV.UI.ImageBox imageBox5;
         private Emgu.CV.UI.ImageBox imageBox6;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
